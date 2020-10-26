10058310654e626ea0d0773f5036b22d9d36bd07
vladislav <vaberezin@protonmail.com>
2020-10-26 15:16:42 +0300

    Changed User.cs. Added Role.cs in Models

--- Models/User.cs
+++ Models/User.cs
@@ -10,6 +10,9 @@ namespace Regulations.Models
         public int Id { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+
+        public int? RoleId { get; set; }
+        public Role Role { get; set; }
         //public string NickName { get; set; }
         //public string FirstName { get; set; }
         //public string LastName { get; set; }
